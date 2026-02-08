b00c3757e406f896e89b92316c092248fc88b576
Amanjit <amanjitsk@cs.toronto.edu>
2026-02-08 00:28:43 -0500

    add copyurl highlight patch

--- config.def.h
+++ config.def.h
@@ -481,7 +481,7 @@ static Shortcut shortcuts[] = {
 	#endif // CLIPBOARD_PATCH
 	{ TERMMOD,              XK_Num_Lock,    numlock,         {.i =  0} },
 	#if COPYURL_PATCH || COPYURL_HIGHLIGHT_SELECTED_URLS_PATCH
-	{ MODKEY,               XK_l,           copyurl,         {.i =  0} },
+	{ TERMMOD,               XK_L,           copyurl,         {.i =  0} },
 	#endif // COPYURL_PATCH
 	#if OPENCOPIED_PATCH
 	{ MODKEY,               XK_o,           opencopied,      {.v = "xdg-open"} },
